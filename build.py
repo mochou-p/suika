5fbd57ff73359164f91f60eeb61efcaa9c34da7b
vrecusko <vrecusko20@gmail.com>
2023-01-28 15:28:55 +0100

    add optimisation and debug info g++ flags

--- build.py
+++ build.py
@@ -7,44 +7,47 @@ from subprocess import run
 from sys        import argv
 
 
-USAGE      = "usage: 'python build.py <flags>'\n" \
-             "flags: c - compile\n"               \
-             "       r - run\n"                   \
-             "       x - clean"
+USAGE        = "usage: 'python build.py <flags>'\n" \
+               "flags: c - compile\n"               \
+               "       r - run\n"                   \
+               "       x - clean"
 
-APP        = "suika"
+APP          = "suika"
 
-BIN        = "./bin"
-INC        = "./inc"
-LIB        = "./lib"
-SRC        = "./src"
+BIN          = "./bin"
+INC          = "./inc"
+LIB          = "./lib"
+SRC          = "./src"
 
-GLFW       = f"{LIB}/GLFW"
-IMGUI      = f"{LIB}/IMGUI"
+GLFW         = f"{LIB}/GLFW"
+IMGUI        = f"{LIB}/IMGUI"
 
-SOURCES    = f"{IMGUI}/*.cpp {SRC}/*.cpp"
-OUTPUT     = f"{BIN}/{APP}"
-HEADERS    = f"-I{INC} -I{GLFW}/include -I{IMGUI}"
-LINKS      = f"-L{GLFW}/lib -lglfw3 -lcomdlg32 -lopengl32 -lgdi32 -mwindows"
+SOURCES      = f"{IMGUI}/*.cpp {SRC}/*.cpp"
+OUTPUT       = f"{BIN}/{APP}"
+HEADERS      = f"-I{INC} -I{GLFW}/include -I{IMGUI}"
+LINKS        = f"-L{GLFW}/lib -lglfw3 -lcomdlg32 -lopengl32 -lgdi32 -mwindows"
 
-STD        = "-std=c++17"
-WARNINGS   = "-Wall -Wextra -Werror"
-EXCEPTIONS = "-Wno-cast-function-type"
+STD          = "-std=c++17"
+OPTIMISATION = "-Og"
+DEBUG        = "-g3"
+WARNINGS     = "-Wall -Wextra -Werror"
+EXCEPTIONS   = "-Wno-cast-function-type"
 
-CXX        = "g++"
-CXXFLAGS   = f"{STD} {WARNINGS} {EXCEPTIONS}"
+CXX          = "g++"
+CXXFLAGS     = f"{STD} {OPTIMISATION} {DEBUG} {WARNINGS} {EXCEPTIONS}"
 
-BUILD      = f"{CXX} {CXXFLAGS} {SOURCES} -o {OUTPUT} {HEADERS} {LINKS}"
+BUILD        = f"{CXX} {CXXFLAGS} {SOURCES} -o {OUTPUT} {HEADERS} {LINKS}"
 
 
-c          = False
-r          = False
-x          = False
-bindir     = False
+c            = False
+r            = False
+x            = False
+bindir       = False
 
 
 def main():
     global c, r, x, bindir
+
     if len(argv) != 2:
         print(USAGE)
         return
@@ -53,7 +56,7 @@ def main():
     r = "r" in argv[1]
     x = "x" in argv[1]
 
-    if not c and not r and not x:
+    if not (c or r or x):
         print(USAGE)
         return
 
